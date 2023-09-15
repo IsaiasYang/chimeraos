f4e1b1443221c6f08d94691cc458d929e2c7c7a7
honjow <honjow311@gmail.com>
2023-09-16 01:34:20 +0800

    fix remove jack2

--- build-image.sh
+++ build-image.sh
@@ -99,15 +99,16 @@ else
 	pacman --noconfirm -S "${KERNEL_PACKAGE}" "${KERNEL_PACKAGE}-headers"
 fi
 
+# install own override packages
+pacman --noconfirm -U --overwrite '*' /own_pkgs/*
+rm -rf /var/cache/pacman/pkg
+
 # pipewire-jack and jack2 are in conflict (jack)
 if pacman -Qs jack2 > /dev/null; then
 	echo "jack2 is installed, removing it"
     pacman -Rdd jack2
 fi
 
-# install own override packages
-pacman --noconfirm -U --overwrite '*' /own_pkgs/*
-rm -rf /var/cache/pacman/pkg
 
 # install packages
 pacman --noconfirm -S --overwrite '*' ${PACKAGES}
